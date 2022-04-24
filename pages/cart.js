d7f04eac51678d1649d8f4f4df9f5a5f22e00cc2
Rian <88561630+RCSJohnson21@users.noreply.github.com>
2022-04-24 13:52:50 -0400

    Update cart.js

--- pages/cart.js
+++ pages/cart.js
@@ -6,10 +6,23 @@ export default function Cart() {
     return (
         <>
             <Menu />
-            <div className="h-96 flex">
-                <span className="m-auto">
-                    My Cart
-                </span>
+            <div class="col-25">
+                <div class="container">
+                    <h2><b>Cart</b>
+                        <span class="price">
+                            <i class="fa fa-shopping-cart"></i>
+                            <b></b>
+                        </span>
+                    </h2>
+                    <p><a href="#">Product1</a> <span class="price">$0.00</span></p>
+                    <p><a href="#">Product2</a> <span class="price">$0.00</span></p>
+                    <p><a href="#">Product3</a> <span class="price">$0.00</span></p>
+
+                    <h2>Total <span class="price"><b>$0.00</b></span></h2>
+                    <a href="/shop" class="continue">Continue Shopping</a>
+                    <a href="/checkout" class="co">Proceed to Checkout</a>
+
+                </div>
             </div>
             <Footer />
         </>
